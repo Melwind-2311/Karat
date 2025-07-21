0e6e9b425c89428fb6e6762c0f40fc81984cd956
Melwind-2311 <melwin.daniel@accenture.com>
2025-07-21 11:59:03 +0530

    Create Day2Case2.cs

---
+++ MyApp/Day2Case2.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < 1000000; i++)
+        {
+            numbers.Add(i % 10000); // Many duplicates
+        }
+
+        HashSet<int> uniqueNumbers = new HashSet<int>();
+
+        foreach (int number in numbers)
+        {
+            uniqueNumbers.Add(number);
+        }
+        Console.WriteLine("Unique count: " + uniqueNumbers.Count);
+    }
+}
