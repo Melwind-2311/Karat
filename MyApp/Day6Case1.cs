9cc38c3d8b853df8f3fc3c2d004bc197219056c6
Melwind-2311 <melwin.daniel@accenture.com>
2025-07-24 13:52:28 +0530

    Update Day6Case1.cs

--- MyApp/Day6Case1.cs
+++ MyApp/Day6Case1.cs
@@ -1,1 +1,35 @@
+using System;
+public class Logger
+{
+    private static Logger instance;
+    private static readonly object lockObj = new object();
+    private Logger() { } 
+ 
+    public static Logger GetInstance() 
+    {
+        lock (lockObj)
+        {
+            if (instance == null) 
+            { 
+                instance = new Logger(); 
+            } 
+            return instance;    
+        }
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Logger log1 = Logger.GetInstance();
+        log1.Log("This is the first message");
+        Logger log2 = Logger.GetInstance();
+        log2.Log("This is second message");
+        Console.WriteLine($"Same Instance? {object.ReferenceEquals(log1, log2)}");
+    }
+}
 
